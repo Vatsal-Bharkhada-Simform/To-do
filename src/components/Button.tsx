5722ac5c06152348cf6934f61efc89693ce0c309
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-26 12:01:30 +0530

    Add: Fexible button component with styles based on variant

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+const primary = "text-white bg-blue-500 rounded-4xl hover:bg-blue-600";
+const secondary = "rounded-lg hover:bg-gray-200";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	variant: "PRIMARY" | "SECONDARY";
+}
+
+const Button: React.FC<ButtonProps> = function (props: ButtonProps) {
+	let buttonStyles: string =
+		"px-4 py-2 cursor-pointer text-sm transition-all ";
+
+	if (props.variant === "PRIMARY") {
+		buttonStyles += primary;
+	} else {
+		buttonStyles += secondary;
+	}
+
+	return (
+		<button
+			className={`${buttonStyles} ${props.className}`}
+			type={props.type}
+			{...props}
+		>
+			{props.children}
+		</button>
+	);
+};
+
+export default Button;
--- src/components/CreateItem.tsx
+++ src/components/CreateItem.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input";
+import Button from "./Button";
 
 export default function CreateItem() {
 	const [task, setTask] = useState("");
@@ -8,19 +9,31 @@ export default function CreateItem() {
 		setTask(e.target.value);
 	}
 
+	function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+		e.preventDefault();
+		console.log("Clicked: ", e);
+	}
+
 	return (
 		<>
 			<header className="">
 				<h1 className="text-2xl font-bold">Your Todo</h1>
 			</header>
 			<div>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<Input
 						placeholder={"Add task"}
 						value={task}
 						key={"TodoInput"}
 						onChange={handleInputChange}
+						name="to-do input"
 					/>
+					<Button
+						variant="PRIMARY"
+						type="submit"
+					>
+						Add task
+					</Button>
 				</form>
 			</div>
 		</>
