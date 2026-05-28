2721b6b6ae0815da3c7d8e5a5862f238163e55db
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-05-28 10:02:51 +0530

    Fix: Logic to clear to-do's on next day

--- src/context/ToDoContextProvider.tsx
+++ src/context/ToDoContextProvider.tsx
@@ -3,10 +3,22 @@ import { ToDoContext } from "./ToDoContext";
 import { todoReducer } from "../utils/todoReducer";
 import { fetchToDO } from "../utils/fetchToDo";
 
-export default function ToDoContextProvider({ children } : {children: ReactElement}) {
+export default function ToDoContextProvider({
+	children,
+}: {
+	children: ReactElement;
+}) {
 	const [toDo, dispatch] = useReducer(todoReducer, fetchToDO());
 
 	useEffect(() => {
+		const prevItems = JSON.parse(localStorage.getItem("TO_DO_ITEMS"));
+		if (
+			prevItems &&
+			prevItems.date !== new Date().toISOString().split("T")[0]
+		) {
+			localStorage.removeItem("TO_DO_ITEMS");
+			return;
+		}
 		localStorage.setItem(
 			"TO_DO_ITEMS",
 			JSON.stringify({
--- src/utils/fetchToDo.ts
+++ src/utils/fetchToDo.ts
@@ -22,7 +22,8 @@ export function fetchToDO(): ToDo[] {
 		}
 
 		return parsedData.toDo.filter(
-			(task: ToDo) => task?.title && task?.id && task?.status && task?.createdAt
+			(task: ToDo) =>
+				task?.title && task?.id && task?.status && task?.createdAt
 		);
 	} catch {
 		return [];
